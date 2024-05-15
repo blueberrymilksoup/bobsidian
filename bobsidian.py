dbd4691024294d6d5b0dc4e8098e812136100855
Samantha <baconspam168@gmail.com>
2024-05-15 03:01:52 -0700

    sidebar works

--- bobsidian.py
+++ bobsidian.py
@@ -26,41 +26,31 @@ def opening():
         text_edit.insert(tk.END, txt)
     main.title(f"BOBSIDIAN - {file_location}")
 
-def deleting():
-    return
-
 def window(type):
      popup = Toplevel(main) # popup window
      popup.geometry("500x600")
      popup.title("BOBSIDIAN Select Note")
 
      if type == "open":
-          option = tk.button(popup, text="option1")
+          option = tk.Button(popup, text="option1")
      elif type == "save":
-          note_name = tk.entry(popup)
-          save_button = tk.button(popup, text="option1")
-
-
-
-def open_window():
-     popup = Toplevel(main) # popup window
-     popup.geometry("500x600")
-     popup.title("BOBSIDIAN Select Note")
-
-def save_window():
-     popup = Toplevel(main) # popup window
-     popup.geometry("500x600")
-     popup.title("BOBSIDIAN Save")
+          popup.title("BOBSIDIAN Enter Information")
+          note_name = tk.Entry(popup)
+          save_button = tk.Button(popup, text="option1")
+     elif type == "delete":
+        return
 
 def hide_sidebar():
-        frames_button.pack_forget()
-        button_sidebar.pack_forget()
-        button_sidebar_s.pack(side=TOP)
+     frames_button.pack_forget()
+     frames_settings.pack_forget()
+     frames_settings_s.pack(side=TOP)
+     button_sidebar_s.pack()
 
 def show_sidebar():
-     button_sidebar.pack(side=TOP)
-     frames_button.pack(side=TOP, expand=TRUE, fill="y")
+     frames_settings_s.pack_forget()
      button_sidebar_s.pack_forget()
+     frames_settings.pack(side=TOP)
+     frames_button.pack(side=TOP, expand=TRUE, fill="y")
      
 
 main = tk.Tk() # makes the window
@@ -70,37 +60,32 @@ main.columnconfigure(1, minsize=800)
 
 popup = Toplevel(main) # popup window
 popup.geometry("500x600")
-popup.title("BOBSIDIAN New Note")
-
+popup.title("BOBSIDIAN Open")
 
 
 text_edit = tk.Text(main, height=100, width=100)
-# text_edit.grid(row=0,column=1, sticky="nsew")
 text_edit.pack(side=RIGHT, expand=TRUE, fill="both")
 
-button_sidebar = tk.Button(main, text="|||", command=hide_sidebar)
-button_sidebar.pack(side=TOP)
-#button_sidebar.grid(row=0, column = 0, padx=5, pady=5)
-
-button_sidebar_s = tk.Button(main, text="|||", command=show_sidebar)
-button_sidebar_s.pack_forget()
-
 # make another frame to put the sidebar buttons in and maybe put a settings button? who knows lmao
+frames_settings = tk.Frame(main,relief = tk.RAISED, bd = 3)
+frames_settings.pack(side=TOP)
+button_sidebar = tk.Button(frames_settings, text="|||", command=hide_sidebar)
+button_sidebar.grid(row=0, column = 0, padx=5, pady=5)
+
+frames_settings_s = tk.Frame(main,relief = tk.RAISED, bd = 3)
+frames_settings_s.pack(side=TOP)
+frames_settings_s.pack_forget()
+button_sidebar_s = tk.Button(frames_settings_s, text="|||", command=show_sidebar)
+button_sidebar.grid(row=0, column = 0, padx=5, pady=5)
 
 
 frames_button = tk.Frame(main, relief = tk.RAISED, bd = 3)
 frames_button.pack(side=TOP, expand=TRUE, fill="y")
-
-#button_sidebar = tk.Button(frames_button, text="|||", command=sidebar)
-#button_sidebar.grid(row=0, column = 0, padx=5, pady=5)
-
-button_open = tk.Button(frames_button, text="Open", command=open_window)
+button_open = tk.Button(frames_button, text="Open", command=lambda: window("open"))
 button_open.grid(row=1, column = 0, padx=5, pady=5)
-
-button_save = tk.Button(frames_button, text="Save", command=save_window)
+button_save = tk.Button(frames_button, text="Save", command=lambda: window("save"))
 button_save.grid(row=2, column = 0, padx=5, pady=5)
-
-button_delete = tk.Button(frames_button, text="Delete", command=delete_window)
+button_delete = tk.Button(frames_button, text="Delete", command=lambda: window("delete"))
 button_delete.grid(row=3, column = 0, padx=5, pady=5)
 
 main.mainloop()
