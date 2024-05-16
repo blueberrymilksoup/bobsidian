51936a42050ef13cdf2192aea09bd03101dadc5f
Samantha <baconspam168@gmail.com>
2024-05-15 21:32:06 -0700

    fixed save

--- bobsidian.py
+++ bobsidian.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter import *
 import os
-
-
+import sys
 
 main_list = "list_of_notes.txt"
 os.chdir("files")
@@ -11,6 +10,8 @@ def saves(name): # create new
     with open(main_list, "a") as new_note:
         new_note.write(name)
     name = name.strip("\n")
+    with open(name, "w") as file_output: # should be able to create a new file 
+        file_output.write("")
     main.title(f"BOBSIDIAN - {name}")
 
 """
@@ -22,7 +23,7 @@ def saver(): # press save button after writing
     with open("current.txt", "r") as reader: # read current
         line_read = reader.readlines()
         note_name = line_read[0]
-        new_name = note_name
+        new_name = note_name.strip('\n')
     with open(new_name, "w") as file_output:
         contents = text_edit.get(1.0, tk.END)
         file_output.write(contents)
@@ -31,8 +32,7 @@ def saver(): # press save button after writing
 def open_delete(selections,type):
         ftuple = get_ftuple(selections)
         index = ftuple[0]
-        note_name = selections.get(index)
-        note_name = note_name.strip("\n") # REMOVE THE \N
+        note_name = selections.get(index).strip("\n") # REMOVE THE \N
         print(note_name)
         if type == "open":
             save_current(note_name)
@@ -105,7 +105,7 @@ def window(type): # will only build the windows and buttons etc
      popup = Toplevel(main) # popup window
      popup.geometry("300x300")
      popup.title("BOBSIDIAN Select Note")
-     
+
      if type == "open": # open note
           popup.geometry("700x700")
           selections = get_selections(popup)
@@ -115,15 +115,14 @@ def window(type): # will only build the windows and buttons etc
      elif type == "delete":
         selections = get_selections(popup)
         ftuple = get_ftuple(selections)
-        real_delete = tk.Button(popup, text="Delete file", command=lambda:[open_delete(selections,"del"), destroy(popup), destroy(selections)])
+        real_delete = tk.Button(popup, text="Open file", command=lambda:[open_delete(selections,"del"), destroy(popup), destroy(selections)])
         real_delete.pack()
-     
+
      elif type == "new":
          popup.title("BOBSIDIAN Enter Information")
          name_entry = tk.Entry(popup)
          name_entry.pack() 
-         the_name = name_entry.get()
-         button_name = tk.Button(popup, text="Start File", command = [checker(the_name), destroy(popup)])
+         button_name = tk.Button(popup, text="Start File", command = lambda: [checker(name_entry.get()),destroy(popup)])
          button_name.pack()
 
 def destroy(item):
@@ -135,18 +134,16 @@ def hide_sidebar():
      frames_settings.pack_forget()
      frames_settings_s.pack(side=TOP)
      button_sidebar_s.pack()
-
 def show_sidebar():
      frames_settings_s.pack_forget()
      button_sidebar_s.pack_forget()
      frames_settings.pack(side=TOP)
      frames_button.pack(side=TOP, expand=TRUE, fill="y")
-    
+
 main = tk.Tk() # makes the window
 main.title("BOBSIDIAN")
 main.rowconfigure(0, minsize=800)
 main.columnconfigure(1, minsize=800)
-
 window_open = Toplevel(main) # popup window to check if you want to open a file or do a new one
 window_open.geometry("500x600")
 window_open.title("BOBSIDIAN Start")
@@ -155,22 +152,19 @@ button_new.pack()
 button_open_start = tk.Button(window_open, text="Open Existing",command=lambda:[window("open"),destroy(window_open)])
 button_open_start.pack()
 
-
 text_edit = tk.Text(main, height=100, width=100)
 text_edit.pack(side=RIGHT, expand=TRUE, fill="both")
-
 # make another frame to put the sidebar buttons in and maybe put a settings button? DONE
 frames_settings = tk.Frame(main,relief = tk.RAISED, bd = 3)
 frames_settings.pack(side=TOP)
 button_sidebar = tk.Button(frames_settings, text="|||", command=hide_sidebar)
 button_sidebar.grid(row=0, column = 0, padx=5, pady=5)
-
 frames_settings_s = tk.Frame(main,relief = tk.RAISED, bd = 3)
 frames_settings_s.pack(side=TOP)
 frames_settings_s.pack_forget()
+
 button_sidebar_s = tk.Button(frames_settings_s, text="|||", command=show_sidebar)
 button_sidebar.grid(row=0, column = 0, padx=5, pady=5)
-
 frames_button = tk.Frame(main, relief = tk.RAISED, bd = 3)
 frames_button.pack(side=TOP, expand=TRUE, fill="y")
 button_open = tk.Button(frames_button, text="Open", command=lambda: window("open"))
@@ -179,8 +173,7 @@ button_save = tk.Button(frames_button, text="Save", command=saver)
 button_save.grid(row=2, column = 0, padx=5, pady=5)
 button_delete = tk.Button(frames_button, text="Delete", command=lambda: window("delete"))
 button_delete.grid(row=3, column = 0, padx=5, pady=5)
-# button_neww = tk.Button(frames_button, text="New", command=lambda: window("new"))
-# button_neww.grid(row=4, column = 0, padx=5, pady=5)
+button_neww = tk.Button(frames_button, text="New", command=lambda: window("new"))
+button_neww.grid(row=4, column = 0, padx=5, pady=5)
 
 main.mainloop()
-
